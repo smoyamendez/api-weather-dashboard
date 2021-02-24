6b603a295ca41c41ae8ee1dded3a91301040a5a1
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-02-23 23:55:19 -0500

    all APIs functioning and layout corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,54 +6,52 @@ var curTemp = document.querySelector('#curTemp');
 var curHumidity = document.querySelector("#curHumidity");
 var curUVIndex = document.querySelector("#curUVIndex");
 var curWindSpeed = document.querySelector("#curWindSpeed");
+var iconEl = document.querySelector("#icon");
 var unit = 'imperial'
 
-cityBtn.addEventListener('click', function(event) {
+
+function getUv(lat, lon) {
+    fetch('http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=')
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data)
+            curUVIndex.textContent = 'UV Index: '+ data.value;
+        })
+}
+
+cityBtn.addEventListener('click', function (event) {
     event.preventDefault();
     // Current Weather
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14'+'&units='+ unit)
-    .then(function (response) {
-        if(response.ok) {
-            return response.json();
-        }
-    })
-    .then(function (data) {
-        console.log(data)
-        //  TODO: pull data to page within .then statement
-        curCityName.textContent = data.name; 
-        curTemp.textContent ='Temperature: ' + parseInt(data.main.temp) + "°F"; 
-        curHumidity.textContent = 'Humidity: ' + parseInt(data.main.temp) + '%'; 
-        curWindSpeed.textContent = 'Wind Speed: ' + parseInt(data.wind.speed) + 'mph'; 
-        
-    })
-    //  TODO: add an alert if input doesn't match any city or is ""
-    
-    var a=lat=data.coord.lat;
-    var b=lon=data.coord.lon;
-    // 5 Day Forecast
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14'+'&units='+ unit)
-    .then(function (response) {
-        if(response.ok) {
-            return response.json();
-        }
-    })
-    .then(function (data) {
-        console.log(data)
-        //  TODO: pull data to page within .then statement
-    })
-    //  TODO: add an alert if input doesn't match any city or is ""
-    
-    // UV Index
-    fetch('http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14'+'&units=')
-    .then(function (response) {
-        if(response.ok) {
-            return response.json();
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=' + unit)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
             }
-    })
-     .then(function (data) {
-         console.log(data)
-        //  TODO: pull data to page within .then statement
-        curUVIndex.textContent = data.name; 
-     })
-    //  TODO: add an alert if input doesn't match any city or is ""
-})           
+        })
+        .then(function (data) {
+            console.log(data)
+            curCityName.textContent = data.name + " " + moment().format("M/D/YYYY");
+            curTemp.textContent = 'Temperature: ' + parseInt(data.main.temp) + "°F";
+            curHumidity.textContent = 'Humidity: ' + parseInt(data.main.temp) + '%';
+            curWindSpeed.textContent = 'Wind Speed: ' + parseInt(data.wind.speed) + 'mph';
+            getUv(data.coord.lat, data.coord.lon)
+            
+        })
+   
+    // 5 Day Forecast
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=' + unit)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data)
+
+        })
+    
+})
