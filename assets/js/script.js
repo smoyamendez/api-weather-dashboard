58084bb314551ca834ca0c181c3a40c98e5315c6
Sommer Moya-Mendez <smoyamendez@gmail.com>
2021-02-24 22:01:23 -0500

    changing grabbing APIs to functions to call with btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,16 @@ var curCityName = document.querySelector('#curCityName');
 var curTemp = document.querySelector('#curTemp');
 var curHumidity = document.querySelector("#curHumidity");
 var curUVIndex = document.querySelector("#curUVIndex");
-var curWindSpeed = document.querySelector("#curWindSpeed");
+var dayOne = document.querySelector("#dayOne");
+var dayTwo = document.querySelector("#dayTwo");
+var dayThree = document.querySelector("#dayThree");
+var dayFour = document.querySelector("#dayFour");
+var dayFive = document.querySelector("#curWindSpeed");
+
 var iconEl = document.querySelector("#icon");
 var unit = 'imperial'
 
+// Function for grabbing UV Index from API
 
 function getUv(lat, lon) {
     fetch('http://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=')
@@ -19,12 +25,42 @@ function getUv(lat, lon) {
         })
         .then(function (data) {
             console.log(data)
-            curUVIndex.textContent = 'UV Index: '+ data.value;
+            curUVIndex.textContent = 'UV Index: ' + data.value;
         })
 }
 
-cityBtn.addEventListener('click', function (event) {
-    event.preventDefault();
+// Function for grabbing 5 Day Forecast
+
+function getFiveDay () {
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=' + unit)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            var time = data.list[0].dt_txt.slice(11);
+            // var date = data.list[i].dt_txt.slice(0, 9);
+            var array = [];
+            console.log(time);
+        //     for (var i = 0; i < 40; i++) {
+        //         if (time.includes('12:00:00')) {
+                    
+                    
+        // }
+        
+        
+        
+        })
+
+}
+
+
+
+// Function for grabbing Current Weather
+
+function getCurrent () {
+    
     // Current Weather
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=' + unit)
         .then(function (response) {
@@ -39,19 +75,31 @@ cityBtn.addEventListener('click', function (event) {
             curHumidity.textContent = 'Humidity: ' + parseInt(data.main.temp) + '%';
             curWindSpeed.textContent = 'Wind Speed: ' + parseInt(data.wind.speed) + 'mph';
             getUv(data.coord.lat, data.coord.lon)
-            
-        })
-   
-    // 5 Day Forecast
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput.value + '&appid=a93efe3c2ecf98f5aeb49553bbf46b14' + '&units=' + unit)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            console.log(data)
-
-        })
     
+        })
+}
+
+// When Submit Button is clicked
+
+cityBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    getCurrent();
+    // getFiveDay();
 })
+    // var fiveDay = [];
+    // for (var i = 0; i < 40; i++) {
+        //     if(data.list[i].dt_txt === moment().hour(12).minute(0).second(0).add(d, 'd').format('YYYY-MM-DD HH:mm:ss')) {
+            //         for (var d =)
+            //     }
+            
+            //     }
+            // console.log(data);
+            
+            // var j = 1;
+            // for (i = 0; i < 40; i++) {
+            //     if (data.list[i].dt_txt === moment().hour(12).minute(0).second(0).add(j, 'd').format('YYYY-MM-DD HH:mm:ss')) {
+        
+            //         j++;
+//                 }
+//     console.log(array);
+// }
